522898a8f77b65b481dee17fdae6cfc8356c31ee
anees028 <rehmananees028@gmail.com>
2025-08-25 14:35:16 +0200

    Employee component with class added and route set for the page.

--- Pages/EmployeeOverview.razor.cs
+++ Pages/EmployeeOverview.razor.cs
@@ -1,7 +1,15 @@
-﻿namespace ShopHRM.Pages
+﻿using BethanysPieShopHRM.Shared.Domain;
+
+namespace ShopHRM.Pages
 {
     //It's a partial class to accompany the EmployeeOverview.razor file.
     public partial class EmployeeOverview
     {
+        public List<Employee>? Employees { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            Employees = Models.MockDataService.Employees;
+        }
     }
 }
