30cc28aec1c42b7e8534a85ebfae3aadd6c23537
anees028 <rehmananees028@gmail.com>
2025-08-26 15:02:22 +0200

    Employee Card Component added and setup for the use.

---
+++ Components/EmployeeCard.razor.cs
@@ -0,0 +1,11 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace ShopHRM.Components
+{
+    public partial class EmployeeCard
+    {
+        [Parameter]   // This property can receive a value from the parent component.
+        public Employee Employee { get; set; } = default!; 
+    }
+}
