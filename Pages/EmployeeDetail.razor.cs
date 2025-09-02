e4d6273f10a5a1324242fa579e0d16c19424368c
anees028 <rehmananees028@gmail.com>
2025-09-02 17:27:57 +0200

    Employee detail component added successfully.

---
+++ Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,21 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+using ShopHRM.Models;
+
+namespace ShopHRM.Pages
+{
+    public partial class EmployeeDetail
+    {
+        [Parameter]
+        public string EmployeeId { get; set; }
+
+        public Employee? Employee { get; set; } = new Employee();
+
+        // This method would typically fetch employee details of single from a MockDataService
+        protected override Task OnInitializedAsync()
+        {
+            Employee = MockDataService.Employees.FirstOrDefault(e => e.EmployeeId == int.Parse(EmployeeId));
+            return base.OnInitializedAsync();
+        }
+    }
+}
