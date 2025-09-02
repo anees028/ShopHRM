c10809c6e42236d0ff9cfb0f3a3f2a12bddf66d5
anees028 <rehmananees028@gmail.com>
2025-09-02 16:45:45 +0200

    Quick View pop functionality added for EmployeeOverview

--- Pages/EmployeeOverview.razor.cs
+++ Pages/EmployeeOverview.razor.cs
@@ -7,9 +7,16 @@ namespace ShopHRM.Pages
     {
         public List<Employee>? Employees { get; set; } = default!;
 
+        private Employee? _selectedEmployee;
+
         protected override void OnInitialized()
         {
             Employees = Models.MockDataService.Employees;
+        }
+
+        public void ShowQuickViewPopup(Employee selectedEmployee)
+        {
+            _selectedEmployee = selectedEmployee;
         }
     }
 }
