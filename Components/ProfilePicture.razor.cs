651ad50af42a41d17de360bbd18928877b44cd93
anees028 <rehmananees028@gmail.com>
2025-09-08 11:44:48 +0200

    Using Render Component as making a child component.

---
+++ Components/ProfilePicture.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ShopHRM.Components
+{
+    public partial class ProfilePicture
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; } // This allows you to pass in custom content to the component
+    }
+}
